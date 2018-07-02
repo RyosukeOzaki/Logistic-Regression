2978014e1d0b05a470016cfeb2df355620c56eee
ozakiryosuke <38748341+RyosukeOzaki@users.noreply.github.com>
2018-07-03 01:53:28 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 import os
+import math
 from model import *
 from load_data import load_data
 from batch import batcher
-
-def evaluate(LG,input_x,label_y):
-    Loss = LG.cross_entropy_function(input_x,label_y)
-    Accuracy = LG.accuracy(input_x,label_y)
-    return Loss,Accuracy
 
 if __name__ == "__main__":
     dirpath = os.path.dirname(__file__)
     #parameter
     vocasize = 195887
-    epoch = 40
+    epoch = 100
     n_in = vocasize
     n_out = 1
     lr = 0.005
     minibatch_size = 32
 
     train_x,train_y,dev_x,dev_y,test_x,test_y = load_data(dirpath,vocasize)
-
+    m = math.ceil(len(train_y)/minibatch_size)
     #train
-    LG = LogisticRegression(n_in,n_out,lr,minibatch_size)
+    model = LogisticRegression(n_in,n_out,lr,minibatch_size)
     print('Start Training..')
     for ep in range(epoch):
+        loss = 0
+        accuracy = 0
         for input_x,label_y in batcher(train_x,train_y,minibatch_size):
-            Loss,Accuracy = evaluate(LG,input_x,label_y)
-            LG.update(input_x,label_y)
-            print("Train | Epoch:{0} | Data Size:{1} | Loss:{2:.3f} | Accuracy:{3:.2f}%".format(ep+1,len(label_y),Loss,Accuracy*100))
-        Loss,Accuracy = evaluate(LG,dev_x,dev_y)
-        print("Dev | Epoch:{0} | Data Size:{1} | Loss:{2:.3f} | Accuracy:{3:.2f}%".format(ep+1,len(dev_y),Loss,Accuracy*100))
+            y = model.forward(input_x)
+            loss += model.cross_entropy_function(y,label_y)
+            accuracy += model.accuracy(y,label_y)
+            delta_w,delta_b = model.backward(input_x,y,label_y)
+            model.update(delta_w,delta_b)
+        print("Train | Epoch:{0} | Data Size:{1} | Loss:{2:.3f} | Accuracy:{3:.2f}".format(ep+1,len(train_x),loss/m,accuracy/m))
+        if ep%10==0:
+            y = model.forward(dev_x)
+            loss = model.cross_entropy_function(y,dev_y)
+            accuracy = model.accuracy(y,dev_y)
+            print("Dev | Epoch:{0} | Data Size:{1} | Loss:{2:.3f} | Accuracy:{3:.2f}".format(ep+1,len(dev_y),loss,accuracy))
 
     #test
-    Loss,Accuracy = evaluate(LG,test_x,test_y)
-    print("Test | Data Size:{1} | Loss:{2:.3f} | Accuracy:{3:.2f}%".format(ep+1,len(test_y),Loss,Accuracy*100))
+    y = model.forward(test_x)
+    loss = model.cross_entropy_function(y,test_y)
+    accuracy = model.accuracy(y,test_y)
+    print("Test | Data Size:{1} | Loss:{2:.3f} | Accuracy:{3:.2f}".format(len(test_y),loss,accuracy))
--- model.py
+++ model.py
@@ -15,27 +15,25 @@ class LogisticRegression(object):
         return 1 / (1 + np.exp(-input_x))
 
     def forward(self,input_x):
-        return self.sigmoid(self.linear(input_x)) >= 0.5
+        return self.sigmoid(self.linear(input_x))
 
-    def backward(self,input_x,label_y):
-        pre_y = self.sigmoid(self.linear(input_x))
-        delta_w = np.dot(input_x.T,(pre_y - label_y))/len(label_y)
-        delta_b = np.sum(pre_y - label_y)/len(label_y)
-        return delta_w,delta_b
-
-    def update(self,input_x,label_y):
-        delta_w,delta_b = self.backward(input_x,label_y)
-        self.W -= delta_w*self.learn_rate
-        self.b -= delta_b*self.learn_rate
-
-
-    def cross_entropy_function(self,input_x,label_y):
+    def cross_entropy_function(self,pre_y,label_y):
         e=0.00001
-        pre_y = self.sigmoid(self.linear(input_x))
         cross_entropy_loss = - np.mean(np.sum(label_y * np.log(pre_y+e) +(1 - label_y) * np.log(1 - pre_y+e),axis=1))
         return cross_entropy_loss
 
-    def accuracy(self,input_x,label_y):
-        pre_y = self.forward(input_x)
+    def backward(self,input_x,pre_y,label_y):
+        delta_w = np.dot(input_x.T,(pre_y - label_y))
+        delta_b = np.sum(pre_y - label_y)/len(label_y)
+        return delta_w,delta_b
+
+    def update(self,delta_w,delta_b):
+        self.W -= delta_w*self.learn_rate
+        self.b -= delta_b*self.learn_rate
+
+    def accuracy(self,pre_y,label_y):
+        pre_y = pre_y > 0.5
         label_y = label_y >= 1
-        return np.sum(pre_y==label_y)/len(label_y)
+        correct = np.sum(pre_y==label_y)
+        accuracy = correct/len(label_y)
+        return accuracy
